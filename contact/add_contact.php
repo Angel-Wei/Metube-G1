0837c1c010aabc1efe6e3b76a5240ff7f55bf813
Xiaohong Zhang <xiaohoz@g.clemson.edu>
2020-11-11 10:33:56 -0500

    update contact page

--- contact/add_contact.php
+++ contact/add_contact.php
@@ -4,12 +4,17 @@ $contact_type = $_GET['contact_type'];
 $accountid1 = $_GET['accountid1'];
 $accountid2 = $_GET['accountid2'];
 
+$result = mysql_query( "select * from account where accountid='$accountid2'" );
+$contact_name = mysql_fetch_row($result)[1];
+
 add_contact($contact_type, $accountid1, $accountid2);
 
 if ($contact_type == '') {
-  echo "Remove from contact list";
+  echo "Removed $contact_name from contact list";
 }
 else {
-  echo "Added to $contact_type list";
+  echo "Added $contact_name to $contact_type list";
 }
- ?>
+
+mysql_free_result($result);
+?>
---
+++ contact/remove_contact.php
@@ -0,0 +1,19 @@
+<?php
+include_once "../function.php";
+$contact_type = $_GET['contact_type'];
+$accountid1 = $_GET['accountid1'];
+$accountid2 = $_GET['accountid2'];
+
+$result = mysql_query( "select * from account where accountid='$accountid2'" );
+$contact_name = mysql_fetch_row($result)[1];
+
+remove_contact($accountid1, $accountid2);
+
+if ($contact_type == 1) {
+  echo "Removed $contact_name from the Family list";
+}
+elseif ($contact_type == 2) {
+  echo "Removed $contact_name from the Friend list";
+}
+mysql_free_result($result);
+?>
